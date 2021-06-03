a01a2959eea86bcfa7e3667533f7dc71517f771a
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-06-03 21:31:09 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import  {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import MasterclassPage from './pages/MasterclassPage';
+import CocktailPage from './pages/CocktailPage';
+import ContactusPage from './pages/ContactusPage';
+import TeamPage from './pages/TeamPage';
 
 ReactDOM.render(
-  <React.StrictMode>   
-    <App />
-  </React.StrictMode>,
+  <>   
+    <Router>
+      <Route path ='/' element= {<HomePage />} />
+      <Route path ='/masterclass' element= {<MasterclassPage />} />
+      <Route path ='/cocktail' element= {<CocktailPage />} />
+      <Route path ='/team' element= {<TeamPage />} />
+      <Route path ='/contact' element= {<ContactusPage />} />
+    </Router>
+  </>,
   document.getElementById('root')
 );
 
