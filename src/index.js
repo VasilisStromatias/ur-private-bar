8ecd79c6da134105b852f082d44909e579af7de9
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-06-03 21:50:34 +0300

    making the navbar and links

--- src/index.js
+++ src/index.js
@@ -10,14 +10,23 @@ import CocktailPage from './pages/CocktailPage';
 import ContactusPage from './pages/ContactusPage';
 import TeamPage from './pages/TeamPage';
 
+import Menu from './pages/components/Menu';
+import Footer from './pages/components/Footer';
+
 ReactDOM.render(
-  <>   
+  <>  
     <Router>
+
+    <Menu />
+
       <Route path ='/' component= {HomePage} exact />
       <Route path ='/masterclass' component= {MasterclassPage} />
       <Route path ='/cocktail' component= {CocktailPage} />
       <Route path ='/team' component= {TeamPage} />
       <Route path ='/contact' component= {ContactusPage} />
+
+    <Footer />
+
     </Router>
   </>,
   document.getElementById('root')
---
+++ src/pages/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link}    from 'react-router-dom';
+
+function Menu () {
+    return(
+        <>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/masterclass'>Masterclass</Link>
+            <Link to='/cocktail'>Cocktail</Link>
+            <Link to='/team'>Team</Link>
+            <Link to='/contact'>Contact us</Link>
+        </nav>
+        </>
+    );
+}
+
+export default Menu;
