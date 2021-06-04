53755d76638ce3b3d4d4576de02c1337fe7dda27
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-06-04 21:20:47 +0300

    Begin the menu and the footer

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import '../../css/Footer.css';
 
 function Footer () {
     return(
         <>
-        <p>This is the Footer</p>
+    <footer className='d-flex justify-content-around align-items-center
+    bg-dark text-light'>
+
+        <div className='contact text-center'>
+            <h2> Contact Info</h2>
+            <p>E-mail : zachos@gmail.com</p>
+            <p>Tel: 210-7896324</p>
+        </div>
+
+        <div className='social text-center'>
+            <h3>Find us on:</h3>
+        
+            <div className='social-icons'> 
+                <p>Social</p>
+                <p>Social</p>
+                <p>Social</p>
+            </div>
+        </div>
+
+    </footer>
+        
         </>
     );
 }
--- src/pages/components/Menu.js
+++ src/pages/components/Menu.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../../css/Menu.css';
 
 function Menu () {
     return(
         <>
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/masterclass'>Masterclass</Link>
-            <Link to='/cocktail'>Cocktail</Link>
-            <Link to='/team'>Team</Link>
-            <Link to='/contact'>Contact us</Link>
-        </nav>
+    <nav className='main-menu d-flex justify-between navbar navbar-dark bg-dark
+    p-0'>
+            <Link to='/' className='navbar-brand mx-5 p-0'>Ur Private Bar</Link>
+
+        <div className='mx-5 '>
+            <Link to='/' className='nav-item mx-3 text-light active'>Home</Link>
+            <Link to='/masterclass' className='nav-item mx-3 text-light'>Masterclass</Link>
+            <Link to='/cocktail' className='nav-item mx-3 text-light'>Cocktail</Link>
+            <Link to='/team' className='nav-item mx-3 text-light'>Team</Link>
+            <Link to='/contact' className='nav-item mx-3 text-light'>Contact us</Link>
+        </div>
+            
+    </nav>
         </>
     );
 }
