9f8323002d0e50a341f57fbcecb2749ef052f346
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-06-09 22:28:34 +0300

    trying cocktail page

--- src/pages/CocktailPage.js
+++ src/pages/CocktailPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import '../css/CocktailPage.css';
 
+import cocktails1 from '../images/cocktails3.png';
+import cocktail from '../images/test.jpg';
+
 function CocktailPage (){
     return (
         <>
-        <h1>This is the CocktailPage</h1>
+        <div className='cocktail-container jura-font container'>
+
+            <div className='image d-flex justify-content-center' >
+                <img src={cocktails1} alt='cocktail-1'/>
+            </div>
+
+            
+            <div className='image d-flex justify-content-around align-items-center'>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+                
+                <div className='d-flex flex-column text-center text-main-yellow'>
+                    <h2>Cocktail Name</h2>
+                    <p>lime, strawberry, gin, olives, vodka</p>
+                </div>
+                    
+            </div>            
+
+        </div>
         </>   
     );
 }
