72a30bd545f07a92809e2153b8762ced89541321
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-06-19 12:24:27 +0300

    build the cocktail page with more options

--- src/pages/CocktailPage.js
+++ src/pages/CocktailPage.js
@@ -6,25 +6,70 @@ import cocktail from '../images/test.jpg';
 function CocktailPage (){
     return (
         <>
-        <div className='cocktail-container jura-font container'>
+    <div className='cocktail-container jura-font container'>
 
             <div className='cocktail-list-title d-flex justify-content-center text-light' >
                 <h1 className='display-4'>Cocktail List</h1>
             </div>
 
+        <div>
             
-            <div className='image d-flex justify-content-around align-items-center'>
+            <div className='image d-flex justify-content-around align-items-center my-5'>
 
                 <img src={cocktail} alt='cocktail-2' className='cocktail'/>
                 
-                <div className='d-flex flex-column text-center text-main-yellow'>
-                    <h2>Cocktail Name</h2>
-                    <p>lime, strawberry, gin, olives, vodka</p>
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
+                </div>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+                
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
                 </div>
                     
-            </div>            
+            </div>  {/*COCKTAILS LIST LINE 1 */}        
+             
 
-        </div>
+            <div className='image d-flex justify-content-around align-items-center my-5'>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
+                </div>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
+                </div>
+
+            </div> {/*COCKTAILS LIST LINE 2 */}
+            <div className='image d-flex justify-content-around align-items-center my-5'>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
+                </div>
+
+                <img src={cocktail} alt='cocktail-2' className='cocktail'/>
+
+                <div className='d-flex flex-column text-center'>
+                    <h2 className='cocktail-title text-main-yellow'>Cocktail Name</h2>
+                    <p className='cocktail-ingredients text-main-light'>lime, strawberry, gin, olives, vodka</p>
+                </div>
+
+                </div> {/*COCKTAILS LIST LINE 3 */}
+        </div> {/* COCKTAILS LIST CONTAINER */}
+    </div> {/* CONTAINER */}
+    
         </>   
     );
 }
