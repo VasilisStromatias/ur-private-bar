9770a7263f0e1de496dd1abd7cdd577178fa4c8d
VasilisStromatias <stromatiasvasilis@gmail.com>
2021-08-08 12:37:16 +0300

    install npm packages
    install mysql and cors from npm

--- server/index.js
+++ server/index.js
@@ -7,40 +7,42 @@ const port = 3002;
 app.use(cors());
 app.use(express.json())
 
-//Route to get all the posts
-app.get ("/api/get", (req,res)=>{
-    db.query("SELECT * FROM products" , (err,result)=>{
-        if(err){
-            console.log(err)
-        }
-    res.send(result)
-    });
-});
+// //Route to get all the posts
+// app.get ("/api/get", (req,res)=>{
+//     db.query("SELECT * FROM products" , (err,result)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//     res.send(result)
+//     });
+// });
 
-// Route to get one product
-app.get("/api/getFromId/:id", (req,res)=>{
-    const id = req.params.id;
+// // Route to get one product
+// app.get("/api/getFromId/:id", (req,res)=>{
+//     const id = req.params.id;
 
-    db.query("SELECT * FROM products WHERE id = ?", id,
-    (err,result)=>{
-        if(err){
-            console.log(err)
-        }
-        res.send(result)
-    });
-});
+//     db.query("SELECT * FROM products WHERE id = ?", id,
+//     (err,result)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         res.send(result)
+//     });
+// });
 
-// Route for decreasing inventory
-app.post("/api/decinv/:id", (req,res)=>{
+// // Route for decreasing inventory
+// app.post("/api/decinv/:id", (req,res)=>{
 
-    const id= req.params.id;
-    db.query("UPDATE products SET inventory = inventory - 1 WHERE id = ?", id , (err,result)=>{
-      if(err){
-          console.log(err)
-      }  
-      console.log(result)
-    });
-});
+//     const id= req.params.id;
+//     db.query("UPDATE products SET inventory = inventory - 1 WHERE id = ?", id , (err,result)=>{
+//       if(err){
+//           console.log(err)
+//       }  
+//       console.log(result)
+//     });
+// });
+
+app.get('/hello', (req,res)=> res.send('Hello!'));
 
 app.listen (port, ()=>{
     console.log (`Server is running on ${port}`)
